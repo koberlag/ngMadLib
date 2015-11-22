129d5224a1f456b77e79c3f0b6ac56256e2d34d1
Kevin Oberlag <koberlag@gmail.com>
2015-11-22 16:10:51 -0600

    added form validation and submission

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 angular.module('myApp',[])
 .controller('MyCtrl', ['$scope', function($scope){
 
-	$scope.personName = 'Alex';
-	$scope.dirtyTask = 'cleaning toilets';
-	$scope.obnoxiousCelebrity = 'Donald Trump';
-	$scope.jobTitle = 'musician';
-	$scope.celebrity = 'Thom Yorke';
-	$scope.hugeNumber = '10003000';
-	$scope.tediousTask = 'washing dishes';
-	$scope.uselessSkill = 'whistle';
-	$scope.adjective = 'smelly';
-	$scope.nominativePronoun = 'he';
-	$scope.possessivePronoun = 'his';
-	$scope.objectivePronoun = 'him';
-	$scope.gender = 'male';
+	$scope.data = {
+		personName : '',
+		dirtyTask : '',
+		obnoxiousCelebrity : '',
+		jobTitle : '',
+		celebrity : '',
+		hugeNumber : '',
+		tediousTask : '',
+		uselessSkill : '',
+		adjective : '',
+		nominativePronoun : '',
+		possessivePronoun : '',
+		objectivePronoun : '',
+		gender : '',
+		showMadLib : false
+	};
 
-	$scope.$watch('gender', function(gender) {
+	$scope.origChoices = angular.copy($scope.data);
+
+	$scope.$watch('data.gender', function(gender) {
 		if(gender == 'male')
 		{
-			$scope.nominativePronoun = 'he';
-			$scope.possessivePronoun = 'his';
-			$scope.objectivePronoun = 'him';
+			$scope.data.nominativePronoun = 'he';
+			$scope.data.possessivePronoun = 'his';
+			$scope.data.objectivePronoun = 'him';
 		}
 		else
 		{
-			$scope.nominativePronoun = 'she';
-			$scope.possessivePronoun = 'her';
-			$scope.objectivePronoun = 'her';
+			$scope.data.nominativePronoun = 'she';
+			$scope.data.possessivePronoun = 'her';
+			$scope.data.objectivePronoun = 'her';
 		}
 	});
+
+	$scope.submit = function(){
+		$scope.data.showMadLib = true;
+	};
+
+	$scope.reset = function(){
+		$scope.data = angular.copy($scope.origChoices);
+		$scope.choicesForm.$setPristine();
+		$scope.choicesForm.$setUntouched();
+	};
 
 }]);
